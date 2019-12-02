642f41d57a82e56fffb83dd4439e86a693e54236
George Beckham <excellentsport@users.noreply.github.com>
2019-12-01 22:00:16 -0800

    fox some pep8 style issues

--- weathercheck.py
+++ weathercheck.py
@@ -1,11 +1,17 @@
 #! python3
 # WeatherCheck.py - grabs weather for current location
 
-import requests, location, time, console, wc_config, webbrowser
+import requests
+import location
+import time
+import console
+import wc_config
+import webbrowser
 
 current_time = int(time.time())
 
-#tries to get the lat and long for the phone. If it fails, data for saved default lat and long is ised instead
+# tries to get the lat and long for the phone. If it fails, data for saved
+# default lat and long is used instead
 try:
     lat_long_dict = location.get_location()
     latitude = lat_long_dict.get('latitude')
@@ -14,22 +20,24 @@ except:
     console.alert('Could not find GPS data')
     latitude = wc_config.default_latitude
     longitude = wc_config.default_longitude
-    lat_long_dict = {'latitude':latitude, 'longitude':longitude}
+    lat_long_dict = {'latitude': latitude, 'longitude': longitude}
 
 location_dict_list = location.reverse_geocode(lat_long_dict)
 
 location_dict = location_dict_list[0]
 
-#get address of current location to display in the alert from lat and long
+# get address of current location to display in the alert from lat and long
 street = location_dict.get('Street')
 city = location_dict.get('City')
 state = location_dict.get('State')
 
 alert_message = street + '\n' + city + ', ' + state + '\n\n'
 
-res = requests.get('https://api.darksky.net/forecast/' + wc_config.api_key + '/' + str(latitude) + ',' + str(longitude) + ',' + str(current_time))
+res = requests.get('https://api.darksky.net/forecast/' + wc_config.api_key +
+                   '/' + str(latitude) + ',' + str(longitude) + ',' +
+                   str(current_time))
 
-#parse json into way more data than necessary, but it might be handy later
+# parse json into way more data than necessary, but it might be handy later
 weather_data = res.json()
 weather_data_current = weather_data.get('currently')
 weather_data_minutely = weather_data.get('minutely')
@@ -45,11 +53,21 @@ wind_speed = weather_data_daily.get('windSpeed')
 precip_chance = weather_data_daily.get('precipProbability')
 
 alert_message = alert_message + \
-'Current Conditions: ' + summary_current + '\n' + 'Current Temp: ' + str(temp_current) + ' degrees' + '\n' + 'High Temp: ' + str(temp_high) + ' degrees' + '\n' + 'Low Temp: ' + str(temp_low) + ' degrees'  + '\n' + 'Average Wind Today: ' + str(wind_speed) + ' mph'  + '\n' + 'Chance of Rain: ' + str(precip_chance)
+        'Current Conditions: ' + summary_current + '\n' + \
+        'Current Temp: ' + str(temp_current) + ' degrees' + '\n' + \
+        'High Temp: ' + str(temp_high) + ' degrees' + '\n' + \
+        'Low Temp: ' + str(temp_low) + ' degrees'  + '\n' + \
+        'Average Wind Today: ' + str(wind_speed) + ' mph'  + '\n' + \
+        'Chance of Rain: ' + str(precip_chance)
 
-close_option = console.alert('Weather Conditions', alert_message, 'Open Dark Sky', 'Close', hide_cancel_button=True)
+close_option = console.alert(
+    'Weather Conditions',
+    alert_message,
+    'Open Dark Sky',
+    'Close',
+    hide_cancel_button=True)
 
 if close_option == 1:
-    #open dark sky app using URL scheme- could be mofified for any weather app
+    # open dark sky app using URL scheme- could be mofified for any weather app
     webbrowser.open('darksky://')
 
